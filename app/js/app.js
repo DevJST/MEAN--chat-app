af511fa0f85e3b583a358b813d047be283a5bf06
FrontEndDevT <mailtoprograms@gmail.com>
2016-01-13 22:42:40 +0100

    Angular routing added

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,17 @@
 'use strict'
 
-angular.module( 'ChatApp', [ 'ChatApp.chatRoom', 'ChatApp.login' ] );
+angular.module( 'ChatApp', [ 'ngRoute', 'ChatApp.chatRoom', 'ChatApp.login' ] );
+
+angular.module( 'ChatApp' ).config( [ '$routeProvider', function( $routeProvider ) {
+    $routeProvider
+        .when( '/join', {
+            templateUrl: 'modules/login/partials/login.html',
+            controller: 'LoginController'
+        })
+        .when( '/chatRoom', {
+            templateUrl: 'modules/chatRoom/partials/chatRoom.html'
+        })
+        .otherwise({
+            redirectTo:'/join'
+        });
+}]);
--- app/modules/login/js/controllers.js
+++ app/modules/login/js/controllers.js
@@ -1,3 +1,8 @@
 'use strict'
 
-angular.module( 'ChatApp.login.controllers', [] );
+angular.module( 'ChatApp.login.controllers', [] ).
+    controller( 'LoginController', [ '$scope', '$location', function( $scope, $location ) {
+        $scope.login = function() {
+            $location.path( '/chatRoom' );
+        };
+    }]);
